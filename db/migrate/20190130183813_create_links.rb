19d41c0f6516326b01045edf0722ba2b97b4703c
samirdev92 <dev.samir92@gmail.com>
2019-01-30 22:09:20 +0100

    first commit

---
+++ db/migrate/20190130183813_create_links.rb
@@ -0,0 +1,9 @@
+class CreateLinks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :links do |t|
+      t.text :content
+      t.belongs_to :user, index: true
+      t.timestamps
+    end
+  end
+end
